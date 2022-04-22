b5480c20922fd0a1561bdf33e9a1a8cf6da23a8c
Geoteo <matteo.giorgi@protonmail.com>
2022-04-22 11:41:34 +0200

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -9,10 +9,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#729fcf", "#2e3436" },    /* "#bbbbbb", "#222222" */
-	[SchemeSel] = { "#2e3436", "#729fcf" },     /* "#eeeeee", "#005577" */
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#729fcf", "#2e3436" },             /* "#bbbbbb", "#222222" */
+	[SchemeSel] = { "#2e3436", "#729fcf" },              /* "#eeeeee", "#005577" */
+	[SchemeSelHighlight] = { "#ff9500", "#3465a4" },     /* "#ffc978", "#005577" */ 
+	[SchemeNormHighlight] = { "#ff9500", "#555753" },    /* "#ffc978", "#222222" */
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
