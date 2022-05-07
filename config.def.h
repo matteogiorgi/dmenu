7b8d5c34564a9b58d99f5346362394e70e820ddf
Matteo Giorgi <matteo.giorgi@protonmail.com>
2022-05-07 02:19:07 +0200

    new colors

--- config.def.h
+++ config.def.h
@@ -4,22 +4,22 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"mononoki Nerd Font:Bold:pixelsize=12"    /* "monospace:size=10" */
+	"mononoki Nerd Font:Regular:pixelsize=12"    /* "monospace:size=10" */
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#729fcf", "#2e3436" },             /* "#bbbbbb", "#222222" */
-	[SchemeSel] = { "#2e3436", "#729fcf" },              /* "#eeeeee", "#005577" */
-	[SchemeSelHighlight] = { "#ff9500", "#3465a4" },     /* "#ffc978", "#005577" */ 
-	[SchemeNormHighlight] = { "#ff9500", "#555753" },    /* "#ffc978", "#222222" */
+	[SchemeNorm] = { "#FFFFFF", "#171421" },             /* "#bbbbbb", "#222222" */
+	[SchemeSel] = { "#171421", "#FFFFFF" },              /* "#eeeeee", "#005577" */
+	[SchemeSelHighlight] = { "#F66151", "#FFFFFF" },     /* "#ffc978", "#005577" */ 
+	[SchemeNormHighlight] = { "#E9AD0C", "#171421" },    /* "#ffc978", "#222222" */
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 22;
+static unsigned int lineheight = 20;
 static unsigned int min_lineheight = 8;
 
 /*
